e9484e8234a5ff0306403ba0011714b121208a0b
Harrison Lingren <harrisonlingren@gmail.com>
2015-10-31 20:11:33 -0400

    init form and scripts

---
+++ scripts/createObject.py
@@ -0,0 +1,18 @@
+import json,httplib
+connection = httplib.HTTPSConnection('api.parse.com', 443)
+connection.connect()
+connection.request('POST', '/1/classes/Item', json.dumps({
+		"description": desc,
+		"donorname": donor,
+		"imageurl": imgurl,
+		"name": name,
+		"price": startPrice,
+		"priceIncrement": 2.5,
+		"qty": "1"
+     }), {
+       "X-Parse-Application-Id": "WhkQetI8nb0HrIykoaNc8LJ9flHIxOvgaXhFXFxm",
+       "X-Parse-REST-API-Key": "G02koccgg9q6RzqwRmpiQDx3QllASet5iW2XbLob",
+       "Content-Type": "application/json"
+     })
+results = json.loads(connection.getresponse().read())
+print results
