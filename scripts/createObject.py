66edae9898ecdcf84af5c4809e5b8981a062996a
Harrison Lingren <harrisonlingren@gmail.com>
2015-10-31 21:03:43 -0400

    added script and form

--- scripts/createObject.py
+++ scripts/createObject.py
@@ -1,4 +1,27 @@
-import json,httplib
+import json,httplib,cgi
+
+formData = cgi.fieldStorage()
+
+if "itemName" not in formData or "itemDesc" not in formData or "itemDonorName" not in formData or "itemPrice" not in formData:
+	print "<h1>Error</h1>"
+	print "Please fill out all the fields!"
+	return
+	
+
+print "<p>name:", formData['itemName'].value, "</p>"
+print "<p>desc:", formData['itemDesc'].value, "</p>"
+print "<p>donor:", formData['itemDonorName'].value, "</p>"
+print "<p>startPrice:", formData['itemPrice'].value, "</p>"
+print "<p>imgurl:", formData['itemImgURL'].value, "</p>"
+
+
+desc = formData.getvalue('itemDesc')
+donor = formData.getvalue('itemDonorName')
+imgurl = formData.getvalue('itemImgURL')
+name = formData.getvalue('itemName')
+startPrice = formData.getvalue('itemPrice')
+
+
 connection = httplib.HTTPSConnection('api.parse.com', 443)
 connection.connect()
 connection.request('POST', '/1/classes/Item', json.dumps({
