dd4855e84cd9025de1ced33dba3fee0aafa8a791
Harrison Lingren <harrisonlingren@gmail.com>
2015-10-31 22:48:25 -0400

    update form script

--- scripts/createObject.py
+++ scripts/createObject.py
@@ -1,52 +1,20 @@
-#!/usr/bin/python
+import json,httplib
 
-import json,httplib,cgi,cgitb
-cgitb.enable()
-
-print "Content-Type: text/html"
-print
-print """
-    <TITLE>CGI script ! Python</TITLE>
-    <H1>This is my first CGI script</H1>
-    Hello, world!
-"""
-
-
-formData = cgi.fieldStorage()
-
-if "itemName" not in formData or "itemDesc" not in formData or "itemDonorName" not in formData or "itemPrice" not in formData:
-	print "<h3>Error</h3>"
-	print "Please fill out all the fields!"
-	return
-
-print "<p>name:", formData['itemName'].value, "</p>"
-print "<p>desc:", formData['itemDesc'].value, "</p>"
-print "<p>donor:", formData['itemDonorName'].value, "</p>"
-print "<p>startPrice:", formData['itemPrice'].value, "</p>"
-print "<p>imgurl:", formData['itemImgURL'].value, "</p>"
-
-
-desc = formData.getvalue('itemDesc')
-donor = formData.getvalue('itemDonorName')
-imgurl = formData.getvalue('itemImgURL')
-name = formData.getvalue('itemName')
-startPrice = formData.getvalue('itemPrice')
-
-
-connection = httplib.HTTPSConnection('api.parse.com', 443)
-connection.connect()
-connection.request('POST', '/1/classes/Item', json.dumps({
-		"description": desc,
-		"donorname": donor,
-		"imageurl": imgurl,
-		"name": name,
-		"price": startPrice,
-		"priceIncrement": 2.5,
-		"qty": "1"
-     }), {
-       "X-Parse-Application-Id": "WhkQetI8nb0HrIykoaNc8LJ9flHIxOvgaXhFXFxm",
-       "X-Parse-REST-API-Key": "G02koccgg9q6RzqwRmpiQDx3QllASet5iW2XbLob",
-       "Content-Type": "application/json"
-     })
-results = json.loads(connection.getresponse().read())
-print results
+def createObject(name,desc,donor,startPrice,imgurl):
+	connection = httplib.HTTPSConnection('api.parse.com', 443)
+	connection.connect()
+	connection.request('POST', '/1/classes/Item', json.dumps({
+			"description": desc,
+			"donorname": donor,
+			"imageurl": imgurl,
+			"name": name,
+			"price": startPrice,
+			"priceIncrement": 2.5,
+			"qty": "1"
+		 }), {
+		   "X-Parse-Application-Id": "WhkQetI8nb0HrIykoaNc8LJ9flHIxOvgaXhFXFxm",
+		   "X-Parse-REST-API-Key": "G02koccgg9q6RzqwRmpiQDx3QllASet5iW2XbLob",
+		   "Content-Type": "application/json"
+		 })
+	results = json.loads(connection.getresponse().read())
+	print results
---
+++ scripts/readForm.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+
+import cgi,cgitb,createObject
+cgitb.enable()
+
+print "Content-Type: text/html"
+print
+print """
+    <TITLE>CGI script ! Python</TITLE>
+    <H1>This is my first CGI script</H1>
+    Hello, world!
+"""
+
+formData = cgi.fieldStorage()
+
+if "itemName" not in formData or "itemDesc" not in formData or "itemDonorName" not in formData or "itemPrice" not in formData:
+	print "<h3>Error</h3>"
+	print "Please fill out all the fields!"
+	return
+
+print "<p>name:", formData['itemName'].value, "</p>"
+print "<p>desc:", formData['itemDesc'].value, "</p>"
+print "<p>donor:", formData['itemDonorName'].value, "</p>"
+print "<p>startPrice:", formData['itemPrice'].value, "</p>"
+print "<p>imgurl:", formData['itemImgURL'].value, "</p>"
+
+desc = formData.getvalue('itemDesc')
+donor = formData.getvalue('itemDonorName')
+imgurl = formData.getvalue('itemImgURL')
+name = formData.getvalue('itemName')
+startPrice = formData.getvalue('itemPrice')
+
+createObject(name,desc,donor,startPrice,imgurl)
